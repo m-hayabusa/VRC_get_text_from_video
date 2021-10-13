106f114c5651173d90ca7bfbb0f0f5874b0b776b
mewl hayabusa <s.hashimoto@hs-sh.net>
2021-10-13 17:29:08 +0900

    くぎりを代替文字から本来の制御文字に変更

--- Parser.cs
+++ Parser.cs
@@ -10,37 +10,41 @@ public class Parser : UdonSharpBehaviour
     private string[] filelist;
     private string[][][] result;
 
-
     public int parse(string input)
     {
-        string[] files = input.Split('␜'); // File List
+        const char FS = (char)28;
+        const char GS = (char)29;
+        const char RS = (char)30;
+        const char US = (char)31;
+
+        string[] files = input.Split(FS); // File List
         result = new string[files.Length][][];
         filelist = new string[files.Length];
 
         for (int filenum = 0; filenum < files.Length; filenum++)
         {
-            string[] file = files[filenum].Split('␝'); // 0: Header, 1: Body
+            string[] file = files[filenum].Split(GS); // 0: Header, 1: Body
 
             if (file.Length < 2) break;
 
-            string[] header = file[0].Split('␞');
+            string[] header = file[0].Split(RS);
 
             if (header.Length < 3) break;
 
             filelist[filenum] = header[0];
             result[filenum] = new string[int.Parse(header[1]) + 1][];
-            result[filenum][0] = header[2].Split('␟'); //0をそのままキー名にします
+            result[filenum][0] = header[2].Split(US); //0をそのままキー名にします
 
             // int cursor = 0;
 
-            string[] body = file[1].Split('␞');
+            string[] body = file[1].Split(RS);
 
             for (int cursor = 0; cursor < int.Parse(header[1]); cursor++)
             {
                 string row = body[cursor];
                 if (row == "") break;
 
-                result[filenum][cursor + 1] = row.Split('␟');
+                result[filenum][cursor + 1] = row.Split(US);
             }
         }
 
