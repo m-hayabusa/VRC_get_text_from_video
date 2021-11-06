309e91f8348036bf1f4e48f72eb4dcb92bef50be
hayabusa <s.hashimoto@hs-sh.net>
2021-11-06 17:25:54 +0900

    リロードボタンをUdon側でもう一段噛ませるように

---
+++ Utils/nsUtil_ReloadButton.cs
@@ -0,0 +1,12 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+
+public class nsUtil_ReloadButton : UdonSharpBehaviour
+{
+    // [HideInInspector]
+    public Video2Str video2Str;
+    public void onClick(){
+        video2Str.reload();
+    }
+}
