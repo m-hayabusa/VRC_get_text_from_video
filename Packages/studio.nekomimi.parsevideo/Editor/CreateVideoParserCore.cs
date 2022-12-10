a43837392654c1de6dc8e65a300cd55f47a11b6e
hayabusa <s.hashimoto@hs-sh.net>
2022-12-10 19:31:25 +0900

    コンテキストメニューからの追加が正しく動いてなかったのを修正

--- Packages/studio.nekomimi.parsevideo/Editor/CreateVideoParserCore.cs
+++ Packages/studio.nekomimi.parsevideo/Editor/CreateVideoParserCore.cs
@@ -6,10 +6,8 @@ public class CreateVideoParserCore
     [MenuItem("GameObject/nSVideoParser", false, 10)]
     public static void Create(MenuCommand menu)
     {
-        Object[] prefab = AssetDatabase.LoadAllAssetsAtPath("Packages/studio.nekomimi.parsevideo/Runtime/nSVideoParser.prefab");
-        if (prefab.Length < 1) return;
-
-        GameObject res = (GameObject)UnityEditor.PrefabUtility.InstantiatePrefab(prefab[0]);
+        GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>("Packages/studio.nekomimi.parsevideo/Runtime/nSVideoParser.prefab");
+        GameObject res = (GameObject) UnityEditor.PrefabUtility.InstantiatePrefab(prefab);
         GameObjectUtility.SetParentAndAlign(res, (GameObject) menu.context);
         Undo.RegisterCreatedObjectUndo(res, "VideoParserCore");
         Selection.activeObject = res;
