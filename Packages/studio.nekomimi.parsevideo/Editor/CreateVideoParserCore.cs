ca1eb778b095c997b8e4932a163df63bec673cd4
hayabusa <s.hashimoto@hs-sh.net>
2022-12-10 18:41:25 +0900

    nSVideoParserをコンテキストメニューからシーンに追加できるように

---
+++ Packages/studio.nekomimi.parsevideo/Editor/CreateVideoParserCore.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class CreateVideoParserCore
+{
+    [MenuItem("GameObject/nSVideoParser", false, 10)]
+    public static void Create(MenuCommand menu)
+    {
+        Object[] prefab = AssetDatabase.LoadAllAssetsAtPath("Packages/studio.nekomimi.parsevideo/Runtime/nSVideoParser.prefab");
+        if (prefab.Length < 1) return;
+
+        GameObject res = (GameObject)UnityEditor.PrefabUtility.InstantiatePrefab(prefab[0]);
+        GameObjectUtility.SetParentAndAlign(res, (GameObject) menu.context);
+        Undo.RegisterCreatedObjectUndo(res, "VideoParserCore");
+        Selection.activeObject = res;
+    }
+}
